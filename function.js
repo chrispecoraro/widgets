f4e9979e867a949c39aea213d095c1bc9843314f
Christopher Pecoraro <chrispecoraro@gmail.com>
2014-03-18 00:06:36 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,33 @@
 <script>
-  function checkForm(jQueryFormInputs){
+ function checkForm(jQueryFormInputs){
     var formValidity=true;
-    $.each(jQueryFormInputs,function (id,item){
-      if (item.validity.valid===false){
-        $(item).css("transition","all 1s ease-in").css("backgroundColor","#fdd");
-        formValidity = false;
-      }
+
+    $.each(jQueryFormInputs,function (id,resItem)
+    {
+          var node;
+          if ($(resItem).attr("data-validationtarget") && $(resItem).attr("data-validationwrapper")){
+              node = $(resItem).closest( $(resItem).attr("data-validationwrapper") ).find( $(resItem).attr("data-validationtarget") );
+          }
+          else if ($(resItem).attr("type")=="radio"){
+              node = $(resItem).parent();
+          }
+          else
+          {
+
+             node = resItem;
+          }
+
+         $(node).removeClass("invalidInput"); //reset background color
+
+          if (resItem.validity.valid===false){
+            $(node).addClass("invalidInput");  //set background color
+            formValidity = false;
+
+          }
+
     });
     return formValidity;
-  }
+}
 </script>
 
 
